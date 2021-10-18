de16281e72b72a820efa2a7607d67f77713f60d6
Ashish Tiwari <linuxguyio@gmail.com>
2021-10-18 11:55:48 +1100

    added user data to webserver instance

--- main.tf
+++ main.tf
@@ -41,10 +41,63 @@ resource "aws_subnet" "private-subnet" {
 }
 
 # Create a security group rule for webserver
+resource "aws_security_group" "webserver-sg" {
+  name        = "webserver-sg"
+  description = "allow http at port 80 and ssh at port 22 on webserver instance"
+  vpc_id      = aws_vpc.vpc-wp-blog.id
 
+  ingress {
+    description = "SSH for webserver"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "allow http inbound traffic to webserver"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+
+  tags = {
+    Name = "webserver"
+  }
+
+}
 # Create a security group rule for database instance
+# By default deny all
+resource "aws_security_group" "database-sg" {
+  name        = "database-sg"
+  description = "security group for database in private subnet"
+
+  tags = {
+    "Name" = "database"
+  }
+}
 
 # Create a webserver instance in public subnet
+resource "aws_instance" "webserver-instance" {
+  ami                         = "ami-05c029a4b57edda9e"
+  instance_type               = var.webserver_instance_type
+  availability_zone           = var.az
+  associate_public_ip_address = true
+  
+
+  user_data = <<EOF
+        #!/bin/bash
+        yum -y update
+        yum install -y httpd
+        systemctl start httpd.service
+        systemctl enable httpd.service
+        echo "<h1>Hello from $(hostname -f)</h1>" > /var/www/html/index.html
+  EOF
+
+
+
+}
 
 # Create a database instance in private subnet
 
