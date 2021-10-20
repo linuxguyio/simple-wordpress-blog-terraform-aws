9e4929410d332767616555aa968d70342808ba8d
Ashish Tiwari <linuxguyio@gmail.com>
2021-10-20 22:24:44 +1100

    added database endpoint

--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,11 @@
 output "ip-address" {
-    description = "IP address of webserver"
-    value = aws_instance.webserver-instance.public_ip
-  
+  description = "IP address of webserver"
+  value       = aws_instance.webserver-instance.public_ip
+
+}
+
+output "database-endpoint" {
+  description = "Endpoint for the database serever"
+  value       = aws_db_instance.blog-database.endpoint
+
 }
